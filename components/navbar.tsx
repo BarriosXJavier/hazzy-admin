cb9092bbf602ac9e59a048e3b7ccea2a7c627208
David Muriithi <muriithid05@gmail.com>
2023-10-04 08:49:08 +0300

    modified:   app/globals.css 	modified:   components/navbar.tsx 	new file:   components/store-switcher.tsx 	new file:   components/ui/command.tsx 	new file:   components/ui/popover.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,14 +3,13 @@
 import { MainNav } from "@/components/main-nav";
 
 import { UserButton } from "@clerk/nextjs";
+import StoreSwitcher from "@/components/store-switcher";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>
-          Switch store
-        </div>
+        <StoreSwitcher />
         <MainNav className="mx-6"/>
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/"/>
